09a586072e6e2ce7d8eb2e7280dc763aac5670d4
Brahma Rishi <brahmarishi@Brahmas-MacBook-Pro.local>
2025-08-27 23:44:59 +0530

    Cloudflare added

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next.js PWA",
+    name: "Wraptron Info Manager",
     short_name: "NextPWA",
     description: "A Progressive Web App built with Next.js",
     start_url: "/",
